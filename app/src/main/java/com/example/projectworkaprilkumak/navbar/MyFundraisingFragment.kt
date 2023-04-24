484478c566e6bd564083cd4c0769003d7e04ffdc
ismoilovashahina <71559428+ismoilovashahina@users.noreply.github.com>
2023-04-24 19:38:09 +0500

    sixth commit

--- app/src/main/java/com/example/projectworkaprilkumak/navbar/MyFundraisingFragment.kt
+++ app/src/main/java/com/example/projectworkaprilkumak/navbar/MyFundraisingFragment.kt
@@ -1,18 +1,63 @@
 package com.example.projectworkaprilkumak.navbar
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.projectworkaprilkumak.R
+import com.example.projectworkaprilkumak.databinding.FragmentMyFundraisingBinding
 
 class MyFundraisingFragment : Fragment() {
- override fun onCreateView(
+    private lateinit var binding: FragmentMyFundraisingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_my_fundraising, container, false)
+        binding = FragmentMyFundraisingBinding.inflate(inflater, container, false)
+
+        binding.addFab.imageTintList = ColorStateList.valueOf(Color.rgb(255, 50, 50))
+
+     return binding.root
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.bookmark -> {
+//                Log.d("MyDataB", "${bookmarkedList}")
+//                val bundle = Bundle()
+//                bundle.putSerializable("bookmarkedList", bookmarkedList as ArrayList)
+                findNavController().navigate(R.id.action_homeFragment_to_bookmarkFragment)
+            }
+            R.id.notification -> {
+                Toast.makeText(requireContext(), "This is notification", Toast.LENGTH_SHORT)
+            }
+            R.id.search ->{
+                findNavController().navigate(R.id.action_homeFragment_to_searchFragment)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 
 }
