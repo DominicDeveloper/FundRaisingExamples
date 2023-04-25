41d351f5025c845607ccaf4a6c51de40b46dfab7
Shahzod010299 <shakhzodtoshboyev010299@gmail.com>
2023-04-25 20:34:39 +0500

    bug fix

--- app/src/main/java/com/example/projectworkaprilkumak/MainActivity.kt
+++ app/src/main/java/com/example/projectworkaprilkumak/MainActivity.kt
@@ -13,88 +13,53 @@ import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.projectworkaprilkumak.Home.HomeFragment
 import com.example.projectworkaprilkumak.actionbar.BookmarkFragment
 import com.example.projectworkaprilkumak.actionbar.NotificationFragment
 import com.example.projectworkaprilkumak.databinding.ActivityMainBinding
-import com.example.projectworkaprilkumak.navbar.CalendarFragment
-import com.example.projectworkaprilkumak.navbar.MyFundraisingFragment
-import com.example.projectworkaprilkumak.navbar.ProfileFragment
-import com.example.projectworkaprilkumak.onboarding.SplashFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-
-        // yana o'sha vaziyat
-
-        val navHost = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
-        navController = navHost.navController
-        binding.navBar.setupWithNavController(navController)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        val navController = navHostFragment.navController
+        val navView: BottomNavigationView = findViewById(R.id.navBar)
+        navView.setupWithNavController(navController)
 
         binding.navBar.setOnItemSelectedListener {
             when(it.itemId){
-                R.id.calendarFragment -> navController.navigate(R.id.action_homeFragment_to_calendarFragment)  //1
-                R.id.myFundraisingFragment -> navController.navigate(R.id.action_homeFragment_to_myFundraisingFragment) //2
-                R.id.chatFragment ->navController.navigate(R.id.action_homeFragment_to_chatFragment)//3
-                R.id.profileFragment -> navController.navigate(R.id.action_homeFragment_to_profileFragment) //4
-
-
-                //faqat shu fragmentlarda  bo'lish kerak navbar, qolgan joylarda invisible bo'lish kk
+                R.id.homeFragment -> findNavController(R.id.navHost).navigate(R.id.homeFragment)
+                R.id.calendarFragment -> findNavController(R.id.navHost).navigate(R.id.calendarFragment)  //1
+                R.id.myFundraisingFragment -> findNavController(R.id.navHost).navigate(R.id.myFundraisingFragment) //2
+                R.id.chatFragment -> findNavController(R.id.navHost).navigate(R.id.chatFragment)//3
+                R.id.profileFragment -> findNavController(R.id.navHost).navigate(R.id.profileFragment) //4
             }
+
             true
         }
 
-        lifecycleScope.launchWhenResumed {
-
-
-        navController.addOnDestinationChangedListener{_, destination,_ ->
+        navController.addOnDestinationChangedListener {_, destination,_ ->
             when(destination.id) {
-                R.id.homeFragment -> showBottomNavBar()
-                R.id.chatFragment -> showBottomNavBar()
-                R.id.profileFragment -> showBottomNavBar()
-                R.id.calendarFragment -> showBottomNavBar()
-                R.id.myFundraisingFragment -> showBottomNavBar() // bu fragmetnlar tori yozidimi nomi
+                R.id.homeFragment -> binding.navBar.visibility = View.VISIBLE
+                R.id.chatFragment -> binding.navBar.visibility = View.VISIBLE
+                R.id.profileFragment -> binding.navBar.visibility = View.VISIBLE
+                R.id.calendarFragment -> binding.navBar.visibility = View.VISIBLE
+                R.id.myFundraisingFragment -> binding.navBar.visibility = View.VISIBLE // bu fragmetnlar tori yozidimi nomi
 
-                // ha, ishlatib koringchi,  endi chiqyapti hamma fragmentda
-
-                // nav bar hmma frag larda chiqyapti +++ ishlamaypti, splashFragmentda chiqdimi, ha
-
-                // yana o'sha-o'sha, hamma fragmentda boru, lekin ham ishlamayapti
-                else -> hideBottomNavBar()
+                else -> binding.navBar.visibility = View.GONE
             }
+
         }
-        }
-        // qaniydi)), uydagilar chaqirib qolishgandi,
-// comentga olingda osha codni
-
-
-
-// qanqa chiqayabdi ui, hamma fragmentni tagida bottom nav bar bor, codiz git da bormi
-        //ha, linkinibering ozimni kompimda ishlatib koray, sizda moslashaolmayabman
-        //ok hozir, bo'ldimi ochidimi, nima ? github tashladim sizga ssilkasini, aha
-
-
 }
-
-    fun showBottomNavBar() {
-        binding.navBar.visibility = View.VISIBLE
-    }
-
-    fun hideBottomNavBar() {
-        binding.navBar.visibility = View.GONE
-    }
 
 }
